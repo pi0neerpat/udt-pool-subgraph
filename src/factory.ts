d07838e4298e1724acc284b650947dd81064272e
Oliver Nordbjerg <hi@notbjerg.me>
2020-09-29 00:51:15 +0200

    Initial

---
+++ src/factory.ts
@@ -0,0 +1,6 @@
+import { CreateUnipoolCall } from '../generated/Factory/Factory'
+
+export function handleNewPool(call: CreateUnipoolCall): void {
+  // TODO(onbjerg): Implement
+  // TODO(onbjerg): Also persist balance proxies
+}
---
+++ src/pool.ts
@@ -0,0 +1,22 @@
+import {
+  Staked,
+  Withdrawn,
+  RewardAdded,
+  RewardPaid
+} from '../generated/templates/Pool/Pool'
+
+export function handleStake(event: Staked): void {
+  // TODO(onbjerg): Implement
+}
+
+export function handleUnstake(event: Withdrawn): void {
+  // TODO(onbjerg): Implement
+}
+
+export function handleRewardAdded(event: RewardAdded): void {
+  // TODO(onbjerg): Implement
+}
+
+export function handleHarvest(event: RewardPaid): void {
+  // TODO(onbjerg): Implement
+}
