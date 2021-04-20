22691caabe63a0e2a0e58410bd97f73a4cce4517
patrick <blockchainbuddha@gmail.com>
2021-04-20 19:51:05 -0400

    building

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,77 +1,31 @@
-import {
-  BigInt,
-  BigDecimal,
-  Address,
-  log
-} from '@graphprotocol/graph-ts'
-import {
-  ERC20
-} from '../generated/Factory/ERC20'
-import {
-  Pair as PairContract
-} from '../generated/Factory/Pair'
-import {
-  UnipoolFactory,
-  Token,
-  Pair
-} from '../generated/schema'
+import { BigInt, BigDecimal, Address, log } from "@graphprotocol/graph-ts";
+import { ERC20 } from "../generated/Pool/ERC20";
+import { UnipoolFactory, Token, Pair } from "../generated/schema";
 
-export let ZERO_BI = BigInt.fromI32(0)
-export let ONE_BI = BigInt.fromI32(1)
-export let ZERO_BD = BigDecimal.fromString('0')
-export let ONE_BD = BigDecimal.fromString('1')
-export let BI_18 = BigInt.fromI32(18)
+export let ZERO_BI = BigInt.fromI32(0);
+export let ONE_BI = BigInt.fromI32(1);
+export let ZERO_BD = BigDecimal.fromString("0");
+export let ONE_BD = BigDecimal.fromString("1");
+export let BI_18 = BigInt.fromI32(18);
 
 export function exponentToBigDecimal(decimals: BigInt): BigDecimal {
-  let bd = BigDecimal.fromString('1')
+  let bd = BigDecimal.fromString("1");
   for (let i = ZERO_BI; i.lt(decimals as BigInt); i = i.plus(ONE_BI)) {
-    bd = bd.times(BigDecimal.fromString('10'))
+    bd = bd.times(BigDecimal.fromString("10"));
   }
-  return bd
-}
-
-export function loadOrCreateFactory(address: Address): UnipoolFactory {
-  let factory = UnipoolFactory.load(address.toHex())
-  if (factory === null) {
-    factory = new UnipoolFactory(address.toHex())
-    factory.poolCount = 0
-    factory.save()
-  }
-  return factory as UnipoolFactory
+  return bd;
 }
 
 export function loadOrCreateToken(address: Address): Token {
-  let token = Token.load(address.toHex())
+  let token = Token.load(address.toHex());
   if (token === null) {
-    let tokenContract = ERC20.bind(address)
+    let tokenContract = ERC20.bind(address);
 
-    token = new Token(address.toHex())
-    token.symbol = tokenContract.symbol()
-    token.name = tokenContract.name()
-    token.decimals = BigInt.fromI32(tokenContract.decimals())
-    token.save()
+    token = new Token(address.toHex());
+    token.symbol = tokenContract.symbol();
+    token.name = tokenContract.name();
+    token.decimals = BigInt.fromI32(tokenContract.decimals());
+    token.save();
   }
-  return token as Token
-}
-
-export function loadOrCreatePair(address: Address): Pair {
-  let pair = Pair.load(address.toHex())
-  if (pair === null) {
-    let pairContract = PairContract.bind(address)
-    pair = new Pair(address.toHex())
-    let token0 = pairContract.try_token0()
-    if (token0.reverted) {
-      log.info('Could not get token0 address from pair', [])
-      return null
-    }
-    let token1 = pairContract.try_token1()
-    if (token1.reverted) {
-      log.info('Could not get token0 address from pair', [])
-      return null
-    }
-    pair.token0 = loadOrCreateToken(token0.value).id
-    pair.token1 = loadOrCreateToken(token1.value).id
-    pair.save()
-  }
-  return pair as Pair
+  return token as Token;
 }
--- src/pool.ts
+++ src/pool.ts
@@ -1,53 +1,45 @@
-import {
-  BigInt,
-  BigDecimal
-} from '@graphprotocol/graph-ts'
+import { BigInt, BigDecimal } from "@graphprotocol/graph-ts";
 import {
   Staked,
   Withdrawn,
   RewardAdded,
   RewardPaid
-} from '../generated/templates/Pool/Pool'
-import {
-  Pool,
-  Token
-} from '../generated/schema'
-import {
-  exponentToBigDecimal
-} from './helpers'
+} from "../generated/Pool/Pool";
+import { Pool, Token } from "../generated/schema";
+import { exponentToBigDecimal } from "./helpers";
 
-const EIGHTEEN_BI = BigInt.fromI32(18)
+const EIGHTEEN_BI = BigInt.fromI32(18);
 
 export function handleStake(event: Staked): void {
-  let pool = Pool.load(event.address.toHex())
+  let pool = Pool.load(event.address.toHex());
   pool.staked = pool.staked.plus(
     event.params.amount.divDecimal(exponentToBigDecimal(EIGHTEEN_BI))
-  )
-  pool.save()
+  );
+  pool.save();
 }
 
 export function handleUnstake(event: Withdrawn): void {
-  let pool = Pool.load(event.address.toHex())
+  let pool = Pool.load(event.address.toHex());
   pool.staked = pool.staked.minus(
     event.params.amount.divDecimal(exponentToBigDecimal(EIGHTEEN_BI))
-  )
-  pool.save()
+  );
+  pool.save();
 }
 
 export function handleRewardAdded(event: RewardAdded): void {
-  let pool = Pool.load(event.address.toHex())
-  let rewardToken = Token.load(pool.rewardToken)
+  let pool = Pool.load(event.address.toHex());
+  let rewardToken = Token.load(pool.rewardToken);
   pool.rewards = pool.rewards.plus(
     event.params.reward.divDecimal(exponentToBigDecimal(rewardToken.decimals))
-  )
-  pool.save()
+  );
+  pool.save();
 }
 
 export function handleHarvest(event: RewardPaid): void {
-  let pool = Pool.load(event.address.toHex())
-  let rewardToken = Token.load(pool.rewardToken)
+  let pool = Pool.load(event.address.toHex());
+  let rewardToken = Token.load(pool.rewardToken);
   pool.rewards = pool.rewards.minus(
     event.params.reward.divDecimal(exponentToBigDecimal(rewardToken.decimals))
-  )
-  pool.save()
+  );
+  pool.save();
 }
